e87fe5f6938bb8b9c1097fe497bb89e417d7babb
Bracrock <dongho@seodonghoui-MacBookAir.local>
2023-08-15 16:08:54 +0900

    2023-08-15

--- edusite/giveu/views.py
+++ edusite/giveu/views.py
@@ -2,5 +2,5 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def main(request):
-    return HttpResponse("이제 일을 시작해볼까")
+    return HttpResponse("이제 일을 시작해볼까\n깃 다루는 것도 이해해야 되네")
 
